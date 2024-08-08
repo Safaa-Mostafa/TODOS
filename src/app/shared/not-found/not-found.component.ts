9b12d9c1867a856b3446cae02667ebed2a0c240d
Safaa-Mostafa <77861720+Safaa-Mostafa@users.noreply.github.com>
2024-08-08 21:06:15 +0300

    Update not-found.component.ts

--- src/app/shared/not-found/not-found.component.ts
+++ src/app/shared/not-found/not-found.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { Router } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
 constructor(private route:Router){}
+open(){
+  this.route.navigateByUrl('home');
+}
 }
